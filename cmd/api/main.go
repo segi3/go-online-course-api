fd40c1ae9a3fdb3ce88120631065a9a499c190d0
Rafi Nizar Abiyyi <rafi.nizar15@gmail.com>
2023-05-06 00:00:02 +0700

    updated main go - using dependency injection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
-	"net/http"
+	mysql "online-course/pkg/db/mysql"
 
 	"github.com/gin-gonic/gin"
+
+	registerHandler "online-course/internal/register/delivery/http"
+	registerUseCase "online-course/internal/register/usecase"
+	userRepository "online-course/internal/user/repository"
+	useUseCase "online-course/internal/user/usecase"
+	mail "online-course/pkg/mail/sendgrid"
 )
 
 func main() {
+
+	db := mysql.DB()
+
 	r := gin.Default()
 
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "test membuat api",
-		})
-	})
+	mail := mail.NewMail()
+	userRepository := userRepository.NewUserRepository(db)
+	userUseCase := useUseCase.NewUserUseCase(userRepository)
+	registerUseCase := registerUseCase.NewRegisterUseCase(userUseCase, mail)
+	registerHandler.NewRegisterHandler(registerUseCase).Route(&r.RouterGroup)
 
 	r.Run()
 }
