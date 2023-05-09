d239a6eac591b890c724f9651b2671fa14d232f5
Rafi Nizar A <ad.rafiniza@xl.co.id>
2023-05-10 01:31:54 +0700

    paginate utils

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -5,6 +5,7 @@ import (
 	oauthDto "online-course/internal/oauth/dto"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func RandomString(length int) string {
@@ -22,4 +23,28 @@ func RandomString(length int) string {
 func GetCurrentUser(ctx *gin.Context) *oauthDto.MapClaimsResponse {
 	user, _ := ctx.Get("user")
 	return user.(*oauthDto.MapClaimsResponse)
+}
+
+func Paginate(offset int, limit int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		page := offset
+
+		// if less than 0, set to 1
+		if page <= 0 {
+			page = 1
+		}
+
+		pageSize := limit
+
+		switch {
+		case pageSize > 100:
+			pageSize = 100
+		case pageSize <= 0:
+			pageSize = 10
+		}
+
+		offset := (page - 1) * limit
+		return db.Offset(offset).Limit(pageSize)
+
+	}
 }
